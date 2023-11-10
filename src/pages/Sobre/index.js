15d1c553e45db6aa14094afdbc4dba4872277d93
Renanluizssx <renanluizssx@gmail.com>
2023-11-10 18:20:00 -0300

    commitado

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -9,6 +9,8 @@ import react from "../../Assets/react.png";
 import bootstrap from "../../Assets/bootstrap.png";
 import photoshop from "../../Assets/photoshop.png";
 import corel from "../../Assets/corel.png";
+import node from "../../Assets/node.png";
+import api from "../../Assets/api.png";
 import "./sobre.css";
 function Sobre() {
   return (
@@ -16,34 +18,56 @@ function Sobre() {
       <nav>
         <Navegacao />
       </nav>
-      <Row>
-        <Col>
-          <h1 className="font-weight-bold">Renan Luiz</h1>
-          <p>
-            Olá, meu nome é Renan, estou estudando programação desde 2021,
-            sempre querendo evoluir e me aprimorar em programação. Possuo foco
-            no Desenvolvimento Web.
-          </p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2>Hard Skills</h2>
-          <img src={html} alt="html" className="imagem-hard-skills" />
-          <img src={css} alt="css" className="imagem-hard-skills" />
-          <img src={js} alt="javascript" className="imagem-hard-skills" />
-          <img
-            src={bootstrap}
-            alt="javascript"
-            className="imagem-hard-skills"
-          />
-          <img src={react} alt="javascript" className="imagem-hard-skills" />
-          <img
-            src={photoshop}
-            alt="javascript"
-            className="imagem-hard-skills"
-          />
-          <img src={corel} alt="javascript" className="imagem-hard-skills" />
+      <Row className="row-descricao align-items-center justify-content-center">
+        <Col xs={11}>
+          <Row>
+            <Col className="text-center">
+              <h1 className="font-weight-bold">Renan Luiz</h1>
+              <p>
+                Olá, meu nome é Renan, estou estudando programação desde 2021,
+                sempre querendo evoluir e me aprimorar em programação. Possuo
+                foco no Desenvolvimento Web.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              xs={12}
+              className="d-flex justify-content-center flex-column align-items-center"
+            >
+              <h2>Tecnologias</h2>
+              <div className="d-flex container-imagens-hard-skills">
+                <img src={html} alt="html" className="imagem-hard-skills" />
+                <img src={css} alt="css" className="imagem-hard-skills" />
+                <img src={js} alt="javascript" className="imagem-hard-skills" />
+                <img
+                  src={bootstrap}
+                  alt="bootstrap"
+                  className="imagem-hard-skills"
+                />
+                <img src={react} alt="react" className="imagem-hard-skills" />
+                <img src={node} alt="node" className="imagem-hard-skills" />
+                <img src={api} alt="api" className="imagem-hard-skills" />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="d-flex justify-content-center flex-column align-items-center">
+              <h2>Programas de Design</h2>
+              <div className="d-flex">
+                <img
+                  src={photoshop}
+                  alt="photoshop"
+                  className="imagem-hard-skills"
+                />
+                <img
+                  src={corel}
+                  alt="corel draw"
+                  className="imagem-hard-skills"
+                />
+              </div>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
