15bb089d5aaf0507bda01772303d346c11e21c34
Renanluizssx <renanluizssx@gmail.com>
2023-12-30 12:10:28 -0300

    commitado

--- src/ColeçaodeDados/index.js
+++ src/ColeçaodeDados/index.js
@@ -12,9 +12,9 @@ export const dadosPortfolio = [
   {
     nome: "Projeto Cardápio",
     imagem: cardapio,
-    titulo: "Tecnologias: React, , useState, Responsivo",
+    titulo: "Tecnologias: React, useState, Responsivo",
     texto:
-      "Projeto onde criei um cardápio de restaurante, onde o usuário pode filtrar por cardápio",
+      "Projeto onde foi criado um cardápio de restaurante, onde o usuário pode filtrar por cardápio",
     urlBuild: "https://projetorestaurantee.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/Projeto-React-Card-pio",
   },
@@ -23,7 +23,7 @@ export const dadosPortfolio = [
     nome: "Projeto Sorveteria",
     imagem: sorveteria,
     titulo: "Tecnologias: React, React-Router-Dom, Responsivo",
-    texto: "Projeto onde criei um site de sorveteria responsivo",
+    texto: "Projeto onde foi criado um site de sorveteria responsivo",
     urlBuild: "https://sorveteriaartesanal.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/Projeto-Sorveteria-React",
   },
@@ -32,7 +32,7 @@ export const dadosPortfolio = [
     imagem: dark,
     titulo: "Tecnologias: React, useState, Context API, Responsivo",
     texto:
-      "Projeto onde criei um dark mode onde o usuário clica no botão acima que ele trocará entre modo claro e escuro.",
+      "Projeto onde foi criado um dark mode onde o usuário clica no botão acima que ele trocará entre modo claro e escuro.",
     urlBuild: "https://darkmodeagencia.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/React-Dark",
   },
@@ -40,7 +40,7 @@ export const dadosPortfolio = [
     nome: "Projeto Barbshop",
     imagem: barbshop,
     titulo: "Tecnologias: React, Sass, useState, Responsivo",
-    texto: "Projeto onde criei uma página inicial de uma barbearia.",
+    texto: "Projeto onde foi criado uma página inicial de uma barbearia.",
     urlBuild: "https://barbeariamodedark.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/Projeto-Barbearia-React",
   },
@@ -49,7 +49,7 @@ export const dadosPortfolio = [
     imagem: carrinho,
     titulo: "Tecnologias: Html, Css, Javascript, Spread, Localstorage",
     texto:
-      "Projeto onde criei um carrinho de compras, onde o usuário digita nome, preco e o valor, e o usuário pode deletar, adicionar, aumentar ou diminuir a quantidade de produtos",
+      "Projeto de um carrinho de compras, onde o usuário digita nome, preco e o valor, e o usuário pode deletar, adicionar, aumentar ou diminuir a quantidade de produtos",
     urlBuild: "https://renanluizssx.github.io/Project-Carrinho/",
     urlGit: "https://github.com/Renanluizssx/Project-Carrinho/tree/main",
   },
@@ -58,7 +58,7 @@ export const dadosPortfolio = [
     imagem: Post,
     titulo: "Tecnologias: Html, Css, Javascript, Spread, Localstorage",
     texto:
-      "Projeto onde fiz um crud, em que o usuário pode adicionar, editar, excluir ou atualizar o seu post.",
+      "Projeto de um site de blog, em que o usuário pode adicionar, editar, excluir ou atualizar o seu post.",
     urlBuild: "https://renanluizssx.github.io/Project-Post/",
     urlGit: "https://github.com/Renanluizssx/Project-Post",
   },
@@ -67,7 +67,7 @@ export const dadosPortfolio = [
     imagem: lista,
     titulo: "Tecnologias: Html, Css, Javascript, Spread, Localstorage",
     texto:
-      "Projeto onde criei uma lista de tarefas, em que o usuário digita e ele salva no banco de dados do navegador utilizando localstorage e operador spread.",
+      "Projeto onde criei uma lista de tarefas, em que o usuário digita suas tarefas e são salvas",
     urlBuild: "https://renanluizssx.github.io/projeto-lista/",
     urlGit: "https://github.com/Renanluizssx/projeto-lista",
   },
@@ -76,7 +76,7 @@ export const dadosPortfolio = [
     imagem: barbearia,
     titulo: "tecnologias: Html, Css, Responsividade",
     texto:
-      "projeto onde copiei essa landing page do figma, landing page responsivo com media queries.",
+      "projeto onde copiei essa landing page do figma, landing page responsiva.",
     urlBuild: "https://renanluizssx.github.io/Projeto-da-Barbearia/",
     urlGit: "https://github.com/Renanluizssx/Projeto-da-Barbearia",
   },
@@ -96,10 +96,9 @@ export const principaisDadosPortfolio = [
   {
     nome: "Projeto Node",
     imagem: apiFatosHistoricos,
-    titulo:
-      "Tecnologias: React, UI - Bootstrap, Context API, Api, Node, Express, Cors",
+    titulo: "Tecnologias: React, UI - Bootstrap, Api, Node, Express, Cors",
     texto:
-      "projeto que criei utilizando uma api de fatos históricos em que o usuário seleciona cada ano entre 1920 e 2020.",
+      "projeto onde o usuário digita entre 1920 e 2020 e é retornado o ano de cada Fato Histórico.",
     urlBuild: "https://apifatoshistoricos.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/Projeto-Fatos-Hist-ricos-React",
   },
