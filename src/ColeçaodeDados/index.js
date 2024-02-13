4cbe21c7f5b34c1d63e013668b1eb2436351aa1f
Renanluizssx <renanluizssx@gmail.com>
2024-02-13 05:53:10 -0300

    commitado

--- src/ColeçaodeDados/index.js
+++ src/ColeçaodeDados/index.js
@@ -8,7 +8,7 @@ import cardapio from "../Assets/projetocardapio.png";
 import carrinho from "../Assets/carrinho.png";
 export const dadosPortfolio = [
   {
-    nome: "menu project",
+    nome: "Menu project",
     imagem: cardapio,
     titulo: "Tecnologies: React, Sass, useState, Responsive",
     texto:
@@ -19,67 +19,66 @@ export const dadosPortfolio = [
   {
     nome: "Dark Mode Project",
     imagem: dark,
-    titulo: "Tecnologias: React, useState, Context API, Responsivo",
+    titulo: "Tecnologies: React, useState, Context API, Responsive",
     texto:
       "Project where a dark mode was implemented, allowing the user to click on the button above to toggle between light and dark mode",
     urlBuild: "https://darkmodeagencia.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/React-Dark",
   },
   {
-    nome: "Projeto Carrinho de Compras",
+    nome: "Shopping cart project",
     imagem: carrinho,
-    titulo: "Tecnologias: Html, Css, Javascript, Spread, Localstorage",
+    titulo: "Tecnologies: Html, Css, Javascript, Spread, Localstorage",
     texto:
       "Project of a shopping cart, where the user inputs the name, price, and quantity of the item, and can delete, add, increase, or decrease the quantity of products.",
     urlBuild: "https://renanluizssx.github.io/Project-Carrinho/",
     urlGit: "https://github.com/Renanluizssx/Project-Carrinho/tree/main",
   },
   {
-    nome: "Projeto Blog",
+    nome: "Blog project",
     imagem: Post,
-    titulo: "Tecnologias: Html, Css, Javascript, Spread, Localstorage",
+    titulo: "Tecnologies: Html, Css, Javascript, Spread, Localstorage",
     texto:
       "Project of a blog website, where the user can add, edit, delete, or update their posts.",
     urlBuild: "https://renanluizssx.github.io/Project-Post/",
     urlGit: "https://github.com/Renanluizssx/Project-Post",
   },
   {
-    nome: "Projeto Listar Tarefa",
+    nome: "To do list project",
     imagem: lista,
-    titulo: "Tecnologias: Html, Css, Javascript, Spread, Localstorage",
+    titulo: "Tecnologies: Html, Css, Javascript, Spread, Localstorage",
     texto:
       "Project where I created a to-do list, enabling the user to input their tasks and save them.",
     urlBuild: "https://renanluizssx.github.io/projeto-lista/",
     urlGit: "https://github.com/Renanluizssx/projeto-lista",
   },
   {
-    nome: "Projeto Barbshop",
+    nome: "Barbshop Project",
     imagem: barbearia,
-    titulo: "tecnologias: Html, Css, Responsividade",
+    titulo: "tecnologies: Html, Css, Responsive",
     texto:
-      "projeto onde copiei essa landing page do figma, landing page responsiva.",
+      "Project where I copied this landing page from Figma, making it responsive.",
     urlBuild: "https://renanluizssx.github.io/Projeto-da-Barbearia/",
     urlGit: "https://github.com/Renanluizssx/Projeto-da-Barbearia",
   },
 ];
 export const principaisDadosPortfolio = [
   {
-    nome: "Projeto Kanye West",
+    nome: "Kanye West Project",
     imagem: biografiaKanye,
     titulo:
-      "Tecnologias: React, UI - Bootstrap, UX, Api, React-Router-Dom e Dados Mocados, Responsivo",
-    texto:
-      "Projeto pessoal que fiz sobre uma biografia de um artista da música.",
+      "Tecnologies: React, UI - Bootstrap, UX, Api, React-Router-Dom, mock, Responsive",
+    texto: "My personal project about a music artist's biography.",
     urlBuild: "https://biografiakanyewest.netlify.app/",
     urlGit:
       "https://github.com/Renanluizssx/Projeto-Kanye-West/tree/main/myapp",
   },
   {
-    nome: "Projeto Node",
+    nome: "historical fact",
     imagem: apiFatosHistoricos,
-    titulo: "Tecnologias: React, UI - Bootstrap, Api, Node, Express, Cors",
+    titulo: "Tecnologies: React, UI - Bootstrap, Api, Node, Express, Cors",
     texto:
-      "projeto onde o usuário digita entre 1920 e 2020 e é retornado o ano de cada Fato Histórico.",
+      "Project where the user inputs a year between 1920 and 2020, and the corresponding historical events for that year are returned.",
     urlBuild: "https://apifatoshistoricos.netlify.app/",
     urlGit: "https://github.com/Renanluizssx/Projeto-Fatos-Hist-ricos-React",
   },
